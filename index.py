f004db2cebbf59de26ca243e06a780b1fb5a80b5
annybeaut <142330028+annybeaut@users.noreply.github.com>
2023-08-15 17:48:24 +0100

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://movie-indol-three.vercel.app")
+URL = "https://movie-indol-three.vercel.app_logs")
 bot = Bot(TOKEN)
 
 
